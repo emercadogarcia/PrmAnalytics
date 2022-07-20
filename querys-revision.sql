6cb872069e44ec3ddd61ae7fad1409b22d7009d6
Edgar Mercado <emercadogarcia@gmail.com>
2022-07-20 10:19:00 -0400

    actualiza query expedientes

--- querys-revision.sql
+++ querys-revision.sql
@@ -6,7 +6,7 @@
 *****************************************************************
 
 /***  EMERCADO: expedientes CERRADO con tareas abiertas *****/
-elect empresa,numero_expediente, TIPO_EXPEDIENTE 
+select empresa,numero_expediente, TIPO_EXPEDIENTE 
 from crmexpedientes_cab where empresa = '004' and TIPO_EXPEDIENTE='04003' AND FECHA_ALTA >TO_DATE('01/01/2021', 'DD/MM/YYYY') AND STATUS_EXPEDIENTE='99'
 
 select NUMERO_EXPEDIENTE,CODIGO_SECUENCIA, count(numero_expediente) nro_tareas
@@ -14,3 +14,4 @@ select NUMERO_EXPEDIENTE,CODIGO_SECUENCIA, count(numero_expediente) nro_tareas
 from crmexpedientes_cab where empresa = '004' AND FECHA_ALTA >TO_DATE('01/01/2021', 'DD/MM/YYYY') AND STATUS_EXPEDIENTE='99')
   group by NUMERO_EXPEDIENTE, CODIGO_SECUENCIA
   /***************/
+
