8df9d056c5b19672256b155e85c6660f8eb108b4
Edgar Mercado G <emercadogarcia@gmail.com>
2023-03-17 14:53:14 -0400

    actualizacin de codigos de trabajo al dia 17/03/2023

--- querys-revision.sql
+++ querys-revision.sql
@@ -30,3 +30,12 @@ SELECT fecha_factura, CLIENTE, IMP_FAC_BRUTO,IMPORTE_FAC_NETO, DTOS_GLOBAL,IMP_D
 FROM FACTURAS_VENTAS
 WHERE EMPRESA='004' AND EJERCICIO=2020 AND NUMERO_SERIE='111' AND NUMERO_FACTURA=2095
 
+
+/**** query para revision de bpm 04003 */
+SELECT distinct cod_empresa, NUMERO_EXPEDIENTE, NUMERO_LINEA, CODIGO_SECUENCIA, EQUIPO_A_REALIZARLO, usuario_a_realizarlo FROM (select crmexpedientes_lin.*, (select c.codigo_empresa from crmexpedientes_cab c where c.empresa= crmexpedientes_lin.empresa and crmexpedientes_lin.numero_expediente=c.numero_expediente) cod_empresa from crmexpedientes_lin) crmexpedientes_lin 
+WHERE EMPRESA='004' AND STATUS_TAREA='01' and codigo_secuencia in ('030','031','040','050','060','070','075') and numero_expediente in (Select numero_expediente from crmexpedientes_cab 	where FECHA_INICIO >=trunc(current_date,'MONTH') and empresa='004' and tipo_expediente='04003' and status_expediente='01') GROUP BY cod_empresa, NUMERO_EXPEDIENTE, NUMERO_LINEA, CODIGO_SECUENCIA,EQUIPO_A_REALIZARLO, usuario_a_realizarlo
+
+SELECT CODIGO_SECUENCIA, EQUIPO_A_REALIZARLO, usuario_a_realizarlo, COUNT(NUMERO_EXPEDIENTE) TTL FROM crmexpedientes_lin 
+WHERE EMPRESA='004' AND STATUS_TAREA='01' and codigo_secuencia in ('030','031','040','050','060','070','075') and numero_expediente in (Select numero_expediente from crmexpedientes_cab 	where FECHA_INICIO >=trunc(current_date,'MONTH') and empresa='004' and tipo_expediente='04003' and status_expediente='01') GROUP BY  CODIGO_SECUENCIA,EQUIPO_A_REALIZARLO, usuario_a_realizarlo
+
+/**** *****/
